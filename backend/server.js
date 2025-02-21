93976ab8e08b54531af2a4e87c662bb31e1dcef2
Alex Lopez <76691426+lopezelpesado@users.noreply.github.com>
2025-02-21 15:46:15 +0000

    add delete product by id route

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,19 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: `Product ${id} deleted` });
+  } catch (error) {
+    res
+      .status(404)
+      .json({ success: false, message: `Product ${id} not found` });
+  }
+});
+
 app.listen(5000, () => {
   connectDB();
   console.log("server started at http://localhost:5000");
