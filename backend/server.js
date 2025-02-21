8d64acb400657f63862056f3652d82508406884c
Alex Lopez <76691426+lopezelpesado@users.noreply.github.com>
2025-02-21 14:51:46 +0000

    add create product route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,31 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import Product from "./models/Product.js";
 
 const app = express();
 
-app.get("/products", (req, res) => {
-  res.send("server ready");
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    console.error("product field not provided:", req.body);
+
+    return res
+      .status(400)
+      .json({ success: false, message: "provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error(`Error creating product: ${error.message}`);
+    res.status(500).json({ success: false, message: "server error" });
+  }
 });
 
 app.listen(5000, () => {
