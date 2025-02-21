a184fb51e74560b4f48b26e6c8314dda3caae97c
Alex Lopez <76691426+lopezelpesado@users.noreply.github.com>
2025-02-21 13:24:51 +0000

    add prettier

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 
 app.listen(5000, () => {
-    console.log("server started at http://localhost:5000");    
-})
-
+  console.log("server started at http://localhost:5000");
+});
