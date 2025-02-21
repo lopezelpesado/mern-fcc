c36023a8076b10a117761c5b7c77582c0ff6ef36
Alex Lopez <76691426+lopezelpesado@users.noreply.github.com>
2025-02-21 13:54:02 +0000

    add mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
+app.get("/products", (req, res) => {
+  res.send("server ready");
+});
+
 app.listen(5000, () => {
+  connectDB();
   console.log("server started at http://localhost:5000");
 });
