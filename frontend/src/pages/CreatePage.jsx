175f5a1cbbe32286b83067e0515815c0e6b31cd2
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2025-02-27 15:31:18 +0000

    add create new product form

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,3 +1,77 @@
-const CreatePage = () => <div>CreatePage</div>;
+import { useColorModeValue } from "@/components/ui/color-mode";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const handleAddProduct = () => {
+    console.log(newProduct);
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <VStack>
+        <Heading as="h1" size="2xl" textAlign="center" mb="8">
+          Create new Product
+        </Heading>
+        <Box
+          w="full"
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack>
+            <Input
+              borderColor={useColorModeValue("gray.800", "white")}
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+
+            <Input
+              borderColor={useColorModeValue("gray.800", "white")}
+              placeholder="Product Price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+
+            <Input
+              borderColor={useColorModeValue("gray.800", "white")}
+              placeholder="Product Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+
+            <Button w="full" onClick={handleAddProduct}>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
+};
 
 export default CreatePage;
