80a4f65ab8b999849bb3fbf655c2ce8c5cc03ffc
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2025-02-28 19:21:51 +0000

    add homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,43 @@
-const HomePage = () => <div>HomePage</div>;
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <Container maxW="xl" py={12}>
+      <VStack>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient="to-r"
+          gradientFrom="cyan.400"
+          gradientTo="cyan.200"
+          bgClip="text"
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+        <SimpleGrid></SimpleGrid>
+
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight="bold"
+          color="gray.500"
+        >
+          No products found 😢{" "}
+          <Link to="/create">
+            <Text
+              as="span"
+              color="blue.500"
+              _hover={{ textDecoration: "underline" }}
+            >
+              Create a product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
 
 export default HomePage;
