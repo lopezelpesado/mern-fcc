200e6ba020f4f2448a0638cceeebda25fc05b55f
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2025-02-28 20:50:23 +0000

    add product card and grid

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,40 +1,64 @@
+import ProductCard from "@/components/ProductCard";
+import { useProductStore } from "@/store/product";
 import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return (
-    <Container maxW="xl" py={12}>
-      <VStack>
-        <Text
-          fontSize={"30"}
-          fontWeight={"bold"}
-          bgGradient="to-r"
-          gradientFrom="cyan.400"
-          gradientTo="cyan.200"
-          bgClip="text"
-          textAlign={"center"}
-        >
-          Current Products 🚀
-        </Text>
-        <SimpleGrid></SimpleGrid>
+  const { fetchProducts, products } = useProductStore();
 
-        <Text
-          fontSize="xl"
-          textAlign={"center"}
-          fontWeight="bold"
-          color="gray.500"
-        >
-          No products found 😢{" "}
-          <Link to="/create">
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  return (
+    <Container py={12}>
+      <VStack>
+        {products.length > 0 ? (
+          <>
             <Text
-              as="span"
-              color="blue.500"
-              _hover={{ textDecoration: "underline" }}
+              fontSize={"30"}
+              fontWeight={"bold"}
+              bgGradient="to-r"
+              gradientFrom="cyan.400"
+              gradientTo="cyan.200"
+              bgClip="text"
+              textAlign={"center"}
             >
-              Create a product
+              Current Products 🚀
             </Text>
-          </Link>
-        </Text>
+            <SimpleGrid
+              columns={{
+                base: 1,
+                md: 2,
+                lg: 3,
+              }}
+              gap={10}
+            >
+              {products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+            </SimpleGrid>
+          </>
+        ) : (
+          <Text
+            fontSize="xl"
+            textAlign={"center"}
+            fontWeight="bold"
+            color="gray.500"
+          >
+            No products found 😢{" "}
+            <Link to="/create">
+              <Text
+                as="span"
+                color="blue.500"
+                _hover={{ textDecoration: "underline" }}
+              >
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        )}
       </VStack>
     </Container>
   );
