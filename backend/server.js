1f71f36aa9a7e83b80d76c6602234fb51d1bbbc7
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2025-03-01 16:30:35 +0000

    add deployment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 import productRoutes from "./routes/product.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "prod") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
